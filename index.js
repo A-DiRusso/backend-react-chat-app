370c329b7fe23b6c96907eb0732d46fc43e15541
Anthony DiRusso <ossurid@gmail.com>
2019-05-02 14:58:49 -0400

    fixes issue with get.'/'

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const db = [
     'Welcome to the chat app'
 ]
 //When Get request comes in send back all the messages
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json(db);
 });
 
 //When Post request comes in add a message to an array of messages.
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
     console.log(req.body.message);
     db.push(req.body.message)
     res.json({
-        // 'message': req.body.message
-        db
+        // 'message': req.body.message // this responds with the message
+        db // this send the whole db back
     })
 });
 const PORT = 31337
