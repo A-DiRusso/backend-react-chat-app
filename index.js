7e65727c1bb7301dc9e90986d69df409131138fe
Anthony DiRusso <ossurid@gmail.com>
2019-05-02 13:56:51 -0400

    adds app.get

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
-app.listen(31337, () => {
-    console.log(`You're app is running at PORT:`)
+
+
+//this is my db
+const db = [
+    'Welcome to the chat app'
+]
+//When Get request comes in send back all the messages
+app.get('/', (req, res) => {
+    res.json(db);
+});
+
+//When Post request comes in add a message to an array of messages.
+
+const PORT = 31337
+app.listen(PORT, () => {
+    console.log(`You're app is running at PORT: ${PORT}`)
 });
