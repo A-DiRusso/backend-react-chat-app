0492bcf616fab6b2c17a32b33f8361350fb43a9e
Anthony DiRusso <ossurid@gmail.com>
2019-05-17 19:13:29 -0400

    adds skeleton for Chatris Backend

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app); //to create a plain vanilla http server that uses our express app
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    server,        // piggy backing on the plain http server 
+    path: '/chat',  // listen on only one route, allowing express to conitue to listen on its custom routes
+});
+
 app.use(express.urlencoded({extended: true}));//extended: true means we can send nested data
 
 //this is my db
 const db = [
     'Welcome to the chat app'
 ]
+
+wss.on('connection', (socket) => {
+    console.log('Oh boy, a new connection!');
+    socket.send(JSON.stringify(db));
+    socket.on('message', (data) => {
+        console.log(data);
+        db.push(data);
+        console.log(db);
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+        // socket.send(data);
+    });
+});
+
 //When Get request comes in send back all the messages
 app.get('/api', (req, res) => {
     res.json(db);
@@ -21,6 +47,6 @@ app.post('/api', (req, res) => {
     })
 });
 const PORT = 31337
-app.listen(PORT, () => {
-    console.log(`You're app is running at PORT: ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`Your app is running at PORT: ${PORT}`)
 });
